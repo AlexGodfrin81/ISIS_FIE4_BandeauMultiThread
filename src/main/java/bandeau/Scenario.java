7c3bfd9186dfc0b70666003dd11df12ae2888e03
AlexGodfrin81 <55447699+AlexGodfrin81@users.noreply.github.com>
2021-12-09 10:04:55 +0100

    Add files via upload

---
+++ src/main/java/bandeau/Scenario.java
@@ -0,0 +1,60 @@
+package bandeau;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Classe utilitaire pour représenter la classe-association UML
+ */
+class ScenarioElement {
+
+    Effect effect;
+    int repeats;
+
+    ScenarioElement(Effect e, int r) {
+        effect = e;
+        repeats = r;
+    }
+}
+/**
+ * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour chaque effet
+ * Un scenario sait se jouer sur un bandeau.
+ */
+public class Scenario {
+
+    private final List<ScenarioElement> myElements = new LinkedList<>();
+    
+    /**
+     * Ajouter un effect au scenario.
+     *
+     * @param e l'effet à ajouter
+     * @param repeats le nombre de répétitions pour cet effet
+     */
+    public void addEffect(Effect e, int repeats) {
+        myElements.add(new ScenarioElement(e, repeats));
+    }
+
+    /**
+     * Jouer ce scenario sur un bandeau
+     *
+     * @param b le bandeau ou s'afficher.
+     * @throws java.lang.InterruptedException
+     */
+    public synchronized void playOn(Bandeau b) {
+        
+        Thread t = new Thread() {
+            public void run(){
+                for (ScenarioElement element : myElements) {
+                    for (int repeats = 0; repeats < element.repeats; repeats++) {
+                        element.effect.playOn(b);
+                    }
+                }
+                System.out.println("Scénario finis");
+            }
+        };
+        t.start();
+    }
+}
